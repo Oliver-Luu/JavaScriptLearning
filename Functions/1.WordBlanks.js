3b7b403e2ee81879af075ac9eeaba71e307b70d1
Oliver-Luu <139717985+Oliver-Luu@users.noreply.github.com>
2024-11-28 12:18:54 -0800

    Use of functions
    Use of functions with a mini "mad-libs" game

---
+++ Functions/1.WordBlanks.js
@@ -0,0 +1,7 @@
+function wordBlanks(noun, adjective, verb, adverb){
+    let result = "";
+    result += "The " + adjective + " " + noun + " " + verb + " " +adverb;
+    return result;
+}
+
+console.log(wordBlanks("fox", "sly", "jumped", "quickly"));
