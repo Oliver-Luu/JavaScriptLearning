bbdf67a0b5e7413e10e81b6c5055836fa38c6390
Oliver-Luu <139717985+Oliver-Luu@users.noreply.github.com>
2025-01-25 23:13:43 -0800

    Uploaded the unique sorting program variants and improvement versions

--- Random Number Generator/randomNumberGenerator.js
+++ Random Number Generator/randomNumberGenerator.js
@@ -4,8 +4,8 @@ console.dir(numbers, {'maxArrayLength': null})
 
 //return {numbers} - Returns 50000 random numbers in a list between 0 and 10000
 function randomNumbersGenerator(){
-    for (let i = 0; i < 50000; i++){
-        numbers[numbers.length]= Math.floor(Math.random() * (10000 - 0 + 1)) + 0;;
+    for (let i = 0; i < 2000; i++){
+        numbers[numbers.length]= Math.floor(Math.random() * (1000 - 0 + 1)) + 0;;
     }
     return numbers;
 }
